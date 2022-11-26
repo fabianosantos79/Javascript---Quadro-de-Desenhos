c0249a8714732e7b846626997bde5e493a6b1b29
Fabiano <fabiano.ssantos79@gmail.com>
2022-11-26 04:16:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let currentColor = 'black';
 let screen = document.querySelector('#tela');
 let ctx = screen.getContext('2d');
 let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
 
 // events
@@ -12,6 +14,7 @@ document.querySelectorAll('.colorArea .color').forEach(item =>{
 screen.addEventListener('mousedown', mouseDownEvent);
 screen.addEventListener('mousemove', mouseMoveEvent);
 screen.addEventListener('mouseup', mouseUpEvent);
+document.querySelector('.clear').addEventListener('click', clearScreen);
 
 
 
@@ -22,20 +25,49 @@ function colorClickEvent(e) {
     document.querySelector('.active').classList.remove('active');
     e.target.classList.add('active');
     console.log("Clicou na cor: ", color);
+
+    currentColor = color;
 }
 
 
 
-function mouseDownEvent() {
+function mouseDownEvent(e) {
     canDraw = true;
+
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
+    
 }
 
-function mouseMoveEvent() {
+function mouseMoveEvent(e) {
     if(canDraw){
-        console.log('DESENHANDO.....');
+        draw(e.pageX, e.pageY);
     }
 }
 
 function mouseUpEvent() {
     canDraw = false;
+}
+
+function draw(x, y) {
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = 'round';
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+
+
+    mouseX = pointX;
+    mouseY = pointY;
+}
+
+function clearScreen() {
+    ctx.setTransform(1,0,0,1,0,0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
